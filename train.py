99f3cfd31be82daf45a9569a6e34722d2becb4dd
fadilparves <fadil.parves@gmail.com>
2021-01-02 23:13:20 +0800

    basic env ready

--- train.py
+++ train.py
@@ -11,3 +11,10 @@ from gym.spaces import Box
 from gym.wrappers import FrameStack
 from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
+
+env = gym_super_mario_bros.make('SuperMarioBros-1-1-v0')
+env = JoypadSpace(env, [['right'], ['right', 'A']])
+env.reset()
+
+next_state, reward, done, info = env.step(action=0)
+print(f"{next_state.shape}, \n {reward}, \n {done}, \n {info}")
