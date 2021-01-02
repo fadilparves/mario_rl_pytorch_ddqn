344a2473f3705f5e7a8cf876483ad9211d8e0c59
fadilparves <fadil.parves@gmail.com>
2021-01-02 22:07:48 +0800

    added libs needed

---
+++ train.py
@@ -0,0 +1,13 @@
+import torch
+from torch import nn
+from torchvision import transforms as T
+from PIL import Image
+import numpy as np
+from pathlib import Path
+from collections import deque
+import random, datetime, os, copy
+import gym
+from gym.spaces import Box
+from gym.wrappers import FrameStack
+from nes_py.wrappers import JoypadSpace
+import gym_super_mario_bros
