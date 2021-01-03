fdd611fd872f6384f8619b2cbaed5b4b50bab43a
fadilparves <fadil.parves@gmail.com>
2021-01-03 21:12:10 +0800

    added skip frame method that skips some frame during the training

--- train.py
+++ train.py
@@ -18,3 +18,24 @@ env.reset()
 
 next_state, reward, done, info = env.step(action=0)
 print(f"{next_state.shape}, \n {reward}, \n {done}, \n {info}")
+
+class SkipFrame(gym.Wrapper):
+    def __init__(self, env, skip):
+        """return only every 'skipped n'-th frame"""
+        super().__init__(env)
+        self._skip = skip
+
+    def step(self, action):
+        """Repeat action, and sum reward"""
+        total_reward = 0.0
+        done = False
+        for i in range(self._skip):
+            # Accumulate reward and repeat the same action
+            obs, reward, done, info = self.env.step(action)
+            total_reward += reward
+            if done:
+                break
+        
+        return obs, total_reward, done, info
+
+
