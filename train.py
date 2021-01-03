382d91bf3bb285c33fefd38fb9a69534612fb421
fadilparves <fadil.parves@gmail.com>
2021-01-03 21:19:02 +0800

    added image resizer, so we will push smaller dim image into the NN

--- train.py
+++ train.py
@@ -55,6 +55,24 @@ class GrayScaleObservation(gym.ObservationWrapper):
         observation = transform(observation)
         return observation
 
+class ResizeObservation(gym.ObservationWrapper):
+    def __init__(self, env, shape):
+        super().__init__(env)
+        if isinstance(shape, int):
+            self.shape = (shape, shape)
+        else:
+            self.shape = tuple(shape)
+
+        obs_shape = self.shape + self.observation_space.shape[2:]
+        self.observation_space = Box(low=0, high=255, shape=obs_shape, dtype=np.uint8)
+
+    def observation(self, observation):
+        transforms = T.Compose(
+            [T.Resize(self.shape), T.Normalize(0,255)]
+        )
+        observation = transforms(observation).squeeze(0)
+        return observation
+
 
 
 
